7d2ac08fb429ce0f5a01ab9e0a07f8c6781a9459
Federico Churca <federico.churca@gmail.com>
2009-12-15 17:52:38 -0300

    Finished amount loading. MUST EXTRACT AMOUNTS FOR INDIVIDUAL OBJECTS

--- report.pas
+++ report.pas
@@ -1,5 +1,5 @@
 program informar;
-uses crt, info, deposito, color, tamano;
+uses crt, info, deposito, color, tamano, objeto;
 
 
 
@@ -12,52 +12,103 @@ Type
 		codigo:char;
 		cantidad:longint;
 	end;
-TablaCcolores = array[1 .. 254] of TICC;
-TablaCtamanos = array['A' .. 'Z'] of TICT;
 
-function cargarcantidades(var archivoC : FTcolor; var archivoT : FTtamano; var archivoD : FTdeposito; var TablaCcolores : TablaCcolores; var TablaCtamanos : TablaCtamanos) : boolean;
-	var
+	TablaCcolores = array[1 .. 254] of TICC;
+	TablaCtamanos = array['A' .. 'Z'] of TICT;
+
+function cargarcantidades(
+	var archivoO : FTobjeto; var archivoC : FTcolor; var archivoT : FTtamano; var archivoD : FTdeposito;
+	var TablaCcolores : TablaCcolores; var TablaCtamanos : TablaCtamanos
+) : boolean;
+	var	
+		TablaCobjetos : TTobjeto;
+		regO : Tobjeto;
+		regD : Tdeposito;
 		regC : Tcolor;
 		regT : Ttamano;
-		i:byte;
-		j:char;
+		i : byte;
+		j, k : char;
+		c : longint;
 		pass : boolean;
 	begin
 		pass := goodFTcolor(archivoC);
-		if not pass then writeln(NO_FILE,PROMPT,COLOURFILE);
+		if not pass then writeln(NO_FILE, PROMPT, COLOURFILE);
 		if pass then begin
 			pass := goodFTtamano(archivoT);
-			if not pass then writeln(NO_FILE,PROMPT,SIZEFILE);
+			if not pass then writeln(NO_FILE, PROMPT, SIZEFILE);
 		end;
 		if pass then begin
 			pass := goodFTdeposito(archivoD);
-			if not pass then writeln(NO_FILE,PROMPT,DEPOTFILE);
+			if not pass then writeln(NO_FILE, PROMPT, DEPOTFILE);
 		end;
 		if pass then begin
-			for i:=1 to 254 do
-			begin
-				tablaCcolores[i].codigo:=0;
-				tablaCcolores[i].cantidad:=0;
+			pass := goodFTobjeto(archivoO);
+			if not pass then writeln(NO_FILE, PROMPT, OBJECTFILE);
+		end;
+		if pass then begin
+			for i:=1 to 254 do begin
+				tablaCcolores[i].codigo := 0;
+				tablaCcolores[i].cantidad := 0;
 			end;
-			for j:='A' to 'Z' do
-			begin
-				tablaCtamanos[j].codigo:=' ';
-				tablaCtamanos[j].cantidad:=0;
+			for j:='A' to 'Z' do begin
+				tablaCtamanos[j].codigo := ' ';
+				tablaCtamanos[j].cantidad := 0;
 			end;
-			while not(eof(archivoC)) do
-			begin
-				read(archivoC,regC);
+
+			while not eof(archivoC) do begin
+				read(archivoC, regC);
 				if goodcolorcode(regC.codigo) then begin
-					tablaCcolores[regC.codigo].codigo:=regC.codigo;
+					tablaCcolores[regC.codigo].codigo := regC.codigo;
 				end else writeln('Color inválido: ', regC.codigo);
 			end;
-			while not(eof(archivoT)) do
-			begin
-				read(archivoT,regT);
+			while not eof(archivoT) do begin
+				read(archivoT, regT);
 				if isalpha(regT.codigo) then begin
 					regT.codigo := toupper(regT.codigo);
-					tablaCtamanos[RegT.codigo].codigo:=RegT.codigo;
+					tablaCtamanos[RegT.codigo].codigo := RegT.codigo;
 				end else writeln('Tamaño inválido: ', regT.codigo);
+			end;
+			loadFTobjeto(archivoO, TablaCobjetos);
+
+			while pass and not eof(archivoD) do begin
+			 read(archivoD, regD);
+			 pass := goodcolorcode(regD.color);
+			 if not pass then writeln('Color inválido para el depósito ', regD.codigo, PROMPT, regD.color);
+			 if pass then begin
+			  pass := goodtamanocode(regD.tamano);
+			  if not pass then writeln('Tamaño inválido para el depósito ', regD.codigo, PROMPT, regD.tamano);
+			 end;
+			 i := 1;
+			 while pass and (i <= DEPOTSIZE) do begin
+			  j := regD.objetos[i].codigo[1];
+			  k := regD.objetos[i].codigo[2];
+			  if (j in ['A'..'Z']) and (k in ['A'..'Z']) then begin
+			   c := regD.objetos[i].cantidad;
+			   if TablaCobjetos[j][k].isactive then begin
+			    if TablaCobjetos[j][k].pos > filesize(archivoO) then begin
+			     writeln('Tabla de objetos inconsistente con ', OBJECTFILE);
+			     pass := false;
+			    end else begin
+			     seek(archivoO, TablaCobjetos[j][k].pos);
+			     read(archivoO, regO);
+			     if (j <> regO.codigo[1]) or (k <> regO.codigo[2]) then begin
+			      writeln('Tabla de objetos inconsistente con ', OBJECTFILE);
+			      pass := false;
+			     end else begin
+			      if regO.tamano in ['A'..'Z'] then
+			       if regO.color in [1..254] then begin
+			        inc(tablaCcolores[regO.color].cantidad, c);
+			        inc(tablaCtamanos[regO.tamano].cantidad, c);
+			       end else writeln('Color inválido para ', j, k, PROMPT, regO.color)
+			      else writeln('Tamaño inválido para ', j, k, PROMPT, regO.tamano);
+			     end;
+			    end;
+			   end
+			   else writeln('Objeto inexistente: ', j, k);
+			  end
+			  else if (j <> '?') or (k <> '?') then
+			   writeln('Objeto inválido: ', j, k);
+			 end;
 			end;
 		end;
 		cargarcantidades := pass;
